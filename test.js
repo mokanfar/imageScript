a06b90b801b3fb7533fe40dc8b12d12c5f6d537e
mokanfar <dannyz1984@gmail.com>
2020-04-10 22:22:57 -0700

    adfadsf

--- test.js
+++ test.js
@@ -9,6 +9,20 @@ let imagePath = path.win32.normalize(process.argv[2]);
 let outputDir = "./rez/";
 process.chdir(imagePath);
 
+listFiles(".")
+  .then((files) => {
+    makeRezDir();
+    return files;
+  })
+  .then((files) => processArray(files))
+  .catch(function (error) {
+    console.error(error);
+  })
+  .then(() => minifyImages())
+  .catch(function (error) {
+    console.error(error);
+  });
+
 async function listFiles(directory) {
   const dirents = await fs.readdir(directory, { withFileTypes: true });
   return dirents
@@ -18,42 +32,35 @@ async function listFiles(directory) {
 async function makeRezDir() {
   try {
     await fs.mkdir(outputDir);
+    //console.log(`created directory rez`);
   } catch (error) {
     console.log(error);
   }
 }
-listFiles(".")
-  .then((files) => {
-    makeRezDir();
-    resizeImages(files);
-  })
-  .then(() => minifyImages())
-  .catch(function (error) {
-    console.error(error);
-  });
+async function processArray(files) {
+  try {
+    for (let img of files) {
+      var dims = sizeOf(img);
+      let settings =
+        dims.height > dims.width ? { height: 1300 } : { width: 2000 };
+      await sharp(img)
+        .resize({
+          ...settings,
+        })
+        .toFile(outputDir + img);
+      //console.log(`processing resize in for loop for image`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function minifyImages() {
   await imagemin([outputDir + "*.jpg"], {
     destination: outputDir + "minified",
-    plugins: [imageminMozjpeg()],
+    plugins: [imageminMozjpeg({ quality: 60 })],
   });
+  //console.log(`minifyImages() function run`);
 }
-const resizeImages = (files) => {
-  for (let img of files) {
-    var dims = sizeOf(img);
-    if (dims.height > dims.width)
-      sharp(img)
-        .resize({
-          height: 1300,
-        })
-        .toFile(outputDir + img);
-    else
-      sharp(img)
-        .resize({
-          width: 2000,
-        })
-        .toFile(outputDir + img);
-  }
-};
 
 //$ node test.js 'C:\z\imageScript\imgs'
